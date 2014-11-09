f26bedb1acdb5ea90780c38d71d5c56db3f2bb91
Alexandra Gurova <alexandragurova@yandex.ru>
2014-11-09 19:37:54 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title ='')
+    default_title = "Microblog App"
+    if page_title.empty?
+      default_title
+    else
+      "#{page_title} | #{default_title}"
+    end
+  end
 end
